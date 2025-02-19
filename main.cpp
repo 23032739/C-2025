7f98dd9ddfb87f53b1ca50001bf29b638f34c923
23032739 <163397290+23032739@users.noreply.github.com>
2025-02-19 19:09:24 +0200

    calculator

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int num1,num2;
+    int sum;
+
+    cout<<"Enter number13:";
+    cin>>num1;
+    cout<<"Enter number2:";
+    cin>>num2;
+
+    sum=num1+num2;
+    cout<<"your answer="<<sum;
+
+    return 0;
+}
